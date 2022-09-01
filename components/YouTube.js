1ad77ee9affbe6be6ce39c1dfd00613cdf5672e5
Cameron <cc44@utas.edu.au>
2022-09-01 18:06:11 +1000

    Fixed display bugs

--- components/YouTube.js
+++ components/YouTube.js
@@ -2,16 +2,13 @@ import getYouTubeId from "get-youtube-id";
 
 const YouTube = ({ url }) => {
   return (
-    <div className="YouTube">
       <iframe
+        className="YouTube"
         title="YouTube Preview"
-        width="560"
-        height="315"
         src={`https://www.youtube.com/embed/${getYouTubeId(url)}`}
-        frameborder="0"
+        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      ></iframe>
-    </div>
+      />
   );
 };
 
--- pages/api/buildRss.js
+++ pages/api/buildRss.js
@@ -1,104 +1,72 @@
 const fs = require("fs");
 const path = require("path");
-const cheerio = require("cheerio");
+import { Feed } from "feed";
 
-function isPageFile(filename) {
-  return path.extname(filename) === ".html" && !filename.endsWith("404.html");
-}
-
-function getPageFiles(directory, files = []) {
-  const entries = fs.readdirSync(directory, { withFileTypes: true });
-  entries.forEach((entry) => {
-    const absolutePath = path.resolve(directory, entry.name);
-    if (entry.isDirectory()) {
-      // wow recursive 🐍
-      getPageFiles(absolutePath, files);
-    } else if (isPageFile(absolutePath)) {
-      files.push(absolutePath);
-    }
-  });
-  return files;
-}
-
-function byDateDesc(a, b) {
-  const date1 = new Date(a.date_published);
-  const date2 = new Date(b.date_published);
-  if (date1 > date2) return -1;
-  if (date1 < date2) return 1;
-  return 0;
-}
-
-function createFeed(pageFiles, pagesDir) {
-    console.log(pagesDir)
-  // use the reduce method to collect all RSS data
-  const rssData = pageFiles.reduce(
-    (data, file) => {
-      // the pathname is the relative path from '/pages' to the HTML file
-      const pathname = path.relative(pagesDir, file).slice(0, -".html".length);
-      // collect all RSS top level info in the index page
-      if (pathname === "index") {
-        const htmlString = fs.readFileSync(file, "utf8");
-        const $ = cheerio.load(htmlString);
-        data.title = $("title").text();
-        data.home_page_url = $(`meta[property='og:url']`).attr("content");
-        data.feed_url = $(
-          `link[rel='alternate'][type='application/json']`
-        ).attr("href");
-        data.description = $(`meta[name='description']`).attr("content");
-        data.icon = $(`link[sizes='512x512']`).attr("href");
-        data.favicon = $(`link[sizes='64x64']`).attr("href");
-      }
-      // only add to RSS if the pathname is '/blog/*'
-      if (pathname.startsWith("blog/") && pathname != "blog/[slug]") {
-        const htmlString = fs.readFileSync(file, "utf8");
-        const $ = cheerio.load(htmlString);
-        // remove the placeholder image for lazy loading images
-        $(`#Content img[aria-hidden='true']`).remove();
-        data.items.push({
-          url: $(`meta[property='og:url']`).attr("content"),
-          id: pathname.substring("blog/".length),
-          content_html: $("#Content").html(),
-          title: $(`meta[property='og:title']`).attr("content"),
-          summary: $(`meta[name='description']`).attr("content"),
-          image: $(`meta[property='og:image']`).attr("content"),
-          banner_image: $(`meta[property='og:image']`).attr("content"),
-          date_published: $("meta[property='og:article:published_time']").attr(
-            "datetime"
-          ),
-          author: {
-            name: $(`meta[property='og:article:author']`).attr("content"),
-            // url: $(`a[rel='author']`).attr("href"),
-            // avatar: $(`img#Avatar`).attr("src"),
-          },
-        });
-      }
-      return data;
-    },
-    {
-      version: "https://jsonfeed.org/version/1",
-      items: [],
-    }
-  );
-  // sort the items by the publishing date
-  rssData.items.sort(byDateDesc);
-  console.log(rssData);
-  // write to the output static folder
-  fs.writeFileSync(
-    path.join("./.next/static", "feed.json"),
-    JSON.stringify(rssData, null, 2)
-  );
-}
+const posts = [
+  {
+    title: "The Gospel Proclaimed",
+    id: "mts-update-june-2022",
+    link: "https://cameronclifford.com/blog/mts-update-june-2022",
+    description:
+      "I'm currently writing most of this update while on the Gold Coast at the moment. I have had the privilege of joining all the other AFES staff at a con...",
+    content:
+      "I'm currently writing most of this update while on the Gold Coast at the moment. I have had the privilege of joining all the other AFES staff at a conference this week. It's been great already to connect with other staff, both new and seasoned, all around Australia (even connecting with some of my old strand leaders from NTE).",
+    date: new Date("2022-06-16T00:00:00"),
+    image:
+      "https://cdn.sanity.io/images/b7iirkjr/production/295a160837d512a42c65017bb79ccf36479d5b07-2240x1260.png",
+  },
+];
 
 export default async function buildRss(req, res) {
-//   // 'pages' location in Vercel environment
-//   let pagesDir = "./.next/serverless/pages";
-//   if (!fs.existsSync(pagesDir)) {
-//     // 'pages' location in local environment
-//     pagesDir = `./.next/server/pages`;
-//   }
-//   const pageFiles = getPageFiles(pagesDir);
-//   createFeed(pageFiles, pagesDir);
-//   console.log("RSS Feed created");
+  const feed = new Feed({
+    title: "Cameron Clifford's Blog",
+    description:
+      "Welcome to my personal website. Check out my blog to get updates on what I am currently thinking and doing.",
+    id: "https://www.cameronclifford.com/",
+    link: "https://www.cameronclifford.com/",
+    language: "en", // optional, used only in RSS 2.0, possible values: http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
+    image: "https://www.cameronclifford.com/portrait.jpg",
+    favicon: "https://www.cameronclifford.com/favicon-64x64.png",
+    copyright: "All rights reserved 2022, Cameron Clifford",
+    updated: new Date(Date.now()), // optional, default = today
+    generator: "Feed for Node.js", // optional, default = 'Feed for Node.js'
+    feedLinks: {
+      json: "https://www.cameronclifford.com/json",
+      atom: "https://www.cameronclifford.com/atom",
+    },
+    author: {
+      name: "Cameron Clifford",
+      email: "mail@cameronclifford.com",
+      link: "https://www.cameronclifford.com",
+    },
+  });
 
-  res.status(200).json({ message: "RSS Feed created" });
+  posts.forEach((post) => {
+    feed.addItem({
+      title: post.title,
+      id: post.url,
+      link: post.url,
+      description: post.description,
+      content: post.content,
+      author: [
+        {
+          name: "Cameron Clifford",
+          email: "mail@cameronclifford.com",
+          link: "https://www.cameronclifford.com",
+        },
+      ],
+      contributor: [],
+      date: post.date,
+      image: post.image,
+    });
+  });
+
+  feed.addCategory("Ministry");
+
+  console.log(feed.rss2());
+  // Output: RSS 2.0
+
+  fs.writeFileSync(path.join("./.next/static", "feed.xml"), feed.rss2());
+
+  res.status(200).json({ message: "RSS Feed created", feed: feed.json1() });
 }
