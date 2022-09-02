cc929478bc7512a7a64922a59c054d3eacf8871a
Cameron <cc44@utas.edu.au>
2022-09-02 22:23:29 +1000

    create rss on postbuild

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -3,8 +3,8 @@ const RSS = require("./buildRss");
 function main() {
   let pagesDir = "./.next/serverless/pages"; // 'pages' location in Vercel environment
 //   let pagesDir = `./.next/server/pages`; // 'pages' location in local environment
-  const pageFiles = getPageFiles(pagesDir);
-  RSS.buildRss(pageFiles, pagesDir);
+//   const pageFiles = getPageFiles(pagesDir);
+  RSS.buildRss();
   console.log("RSS Feed created");
   //   buildSiteMap(pageFiles);
   //   console.log("Site Map created");
