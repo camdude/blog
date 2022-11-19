e0e0b41d1cb1491e3cccbfcc8b21a2900e72f310
Cameron <cc44@utas.edu.au>
2022-11-19 20:22:43 +1100

    updated support page text

--- pages/support.js
+++ pages/support.js
@@ -18,10 +18,17 @@ export default function Subscribe({ supportData }) {
       <Section color="secondary">
         <h2 className="heading-secondary">Ministry Partnership</h2>
         <p className="paragraph">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad sapiente
-          autem, quae aliquam odio earum voluptas quo distinctio maxime quod
-          porro omnis voluptatibus quam debitis nihil nobis molestias? Odit,
-          harum.
+          I am currently seeking people to support me financially to start
+          working with{" "}
+          <a className="link" href="https://afes.org.au/" target="_blank">
+            AFES
+          </a>{" "}
+          in 2023. I'm hoping to be able to support the Launceston students on
+          campus 2 days a week. Please prayerfully consider if you would be able
+          to make a regular pledge.
+        </p>
+        <p className="paragraph">
+          You can see below how I am currently going in my support.
         </p>
         <SupportGraph
           heading={`Monthly Target: $${(supportData.target / 12).toLocaleString(
